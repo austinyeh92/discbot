e249329e26438b5c033e45f68d343cea7bd6e8d8
Austin <aus20030527@gmail.com>
2021-07-20 21:48:06 +0800

    settings.json and .env
    Deleted .env and added settings.json

--- bot.py
+++ bot.py
@@ -1,10 +1,11 @@
-import os
 import discord
 from discord.ext import commands
-from dotenv import load_dotenv
+import json
 
-load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
+with open('settings.json', 'r', encoding='utf8') as jfile:
+    jdata = json.load(jfile)
+
+TOKEN = jdata["DISCORD_TOKEN"]
 
 intents = discord.Intents.default()
 intents.members = True
@@ -17,19 +18,24 @@ async def on_ready():
 
 @bot.event
 async def on_member_join(member):
-    channel = bot.get_channel(867032709263130654)
+    channel = bot.get_channel(jdata["welcome channel"])
     await channel.send(f'{member} joined!')
     print(f'{member} joined!')
 
 @bot.event
 async def on_member_remove(member):
-    channel = bot.get_channel(867032894882185276)
+    channel = bot.get_channel(jdata["those-who-hate-the-server channel"])
     await channel.send(f'{member} left!')
     print(f'{member} left!')
 
-@bot.command('sup')
+@bot.command()
 async def test(ctx):
     await ctx.send(f'Sup! {ctx.author}')
     await ctx.send('How\'s it going?')
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send('Pong')
+    await ctx.send('And stfu')
 
 bot.run(TOKEN)
