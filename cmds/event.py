793b83a364f65f76f0644dc8ac4af1c49aef5038
Austin <aus20030527@gmail.com>
2021-07-24 10:01:04 +0800

    Reaction Roles Done

--- cmds/event.py
+++ cmds/event.py
@@ -28,50 +28,57 @@ class Event(Cog_Extension):
     # Listener in the knighting ceremonies channel
     @commands.Cog.listener()
     async def on_message(self, msg):
-        if msg.channel == self.bot.get_channel(867669689953681429):
-            if msg.content.lower() == 'knight me as the royal warrior':
-                role = discord.utils.get(msg.author.guild.roles, name="The Royal Warrior")
-                await msg.author.add_roles(role)
-                await self.bot.get_channel(867995199161397289).send(f'{msg.author} knighted as the Royal Warrior')
-                await msg.delete()
-            elif msg.content.lower() == 'knight me as bloody miner':
-                role = discord.utils.get(msg.author.guild.roles, name="Bloody Miner")
-                await msg.author.add_roles(role)
-                await self.bot.get_channel(867995199161397289).send(f'{msg.author} knighted as Bloody Miner')
-                await msg.delete()
-            elif msg.content.lower() == 'knight me as pixel protector':
-                role = discord.utils.get(msg.author.guild.roles, name="Pixel Protector")
-                await msg.author.add_roles(role)
-                await self.bot.get_channel(867995199161397289).send(f'{msg.author} knighted as Pixel Protector')
-                await msg.delete()
-            elif msg.content.lower() == 'take my roles if you really have to':
-                role = discord.utils.get(msg.author.guild.roles, name="Peasant")
-                await msg.author.edit(roles=[role])
-                await self.bot.get_channel(867995199161397289).send(f'{msg.author}\'s all roles taken')
-                await msg.delete()
-            else:
-                channel = msg.channel
-                await msg.delete()
+        pass
 
     @commands.Cog.listener()
     async def on_raw_reaction_add(self, payload):
-        user = self.bot.get_guild(payload.guild_id).get_member(payload.user_id)
-        channel = self.bot.get_channel(867429179850227753)
-        msg = await channel.fetch_message(868127572616183829)
-        if payload.message_id == 868127572616183829 and user.top_role == discord.utils.get(user.guild.roles, name="Trespasser"):
-            role = discord.utils.get(user.guild.roles, name="Peasant")
-            await user.edit(roles=[role])
-            await self.bot.get_channel(867995199161397289).send(f'{user} reacted to the rules and became a Peasant')
+        member = self.bot.get_guild(payload.guild_id).get_member(payload.user_id)
+        channel = self.bot.get_channel(payload.channel_id) 
+        msg = await channel.fetch_message(payload.message_id) 
+        emoji = payload.emoji.name
+        if payload.channel_id == 867429179850227753 and payload.message_id == 868127572616183829 and member.top_role == discord.utils.get(member.guild.roles, name="Trespasser"):
+            role = discord.utils.get(member.guild.roles, name="Peasant")
+            await member.edit(roles=[role])
+            await self.bot.get_channel(867995199161397289).send(f'{member} reacted to the rules and became a Peasant')
+        
+        if payload.channel_id == 867669689953681429 and payload.message_id == 867693325372358666:
+            if emoji == 'piggy_warrior':
+                role = discord.utils.get(member.guild.roles, name="The Royal Warrior")
+                await member.add_roles(role)
+                await self.bot.get_channel(867995199161397289).send(f'{member} was knighted The Royal Warrior')
+            elif emoji == 'spinningminecraftsteve':
+                role = discord.utils.get(member.guild.roles, name="Pixel Protector")
+                await member.add_roles(role)
+                await self.bot.get_channel(867995199161397289).send(f'{member} was knighted Pixel Protector')
+            elif emoji == 'PikaPickaxe':
+                role = discord.utils.get(member.guild.roles, name="Bloody Miner")
+                await member.add_roles(role)
+                await self.bot.get_channel(867995199161397289).send(f'{member} was knighted Bloody Miner')
 
     @commands.Cog.listener()
     async def on_raw_reaction_remove(self, payload):
-        user = self.bot.get_guild(payload.guild_id).get_member(payload.user_id)
-        channel = self.bot.get_channel(867429179850227753)
-        msg = await channel.fetch_message(868127572616183829)
-        if payload.message_id == 868127572616183829:
-            role = discord.utils.get(user.guild.roles, name="Trespasser")
-            await user.edit(roles=[role])
-            await self.bot.get_channel(867995199161397289).send(f'{user} decided not to accept the rules and became a Trespasser')
+        member = self.bot.get_guild(payload.guild_id).get_member(payload.user_id)
+        channel = self.bot.get_channel(payload.channel_id) 
+        msg = await channel.fetch_message(payload.message_id) 
+        emoji = payload.emoji.name
+        if payload.channel_id == 867429179850227753 and payload.message_id == 868127572616183829:
+            role = discord.utils.get(member.guild.roles, name="Trespasser")
+            await member.edit(roles=[role])
+            await self.bot.get_channel(867995199161397289).send(f'{member} decided not to accept the rules and became a Trespasser')
+
+        if payload.channel_id == 867669689953681429 and payload.message_id == 867693325372358666:
+            if emoji == 'piggy_warrior':
+                role = discord.utils.get(member.guild.roles, name="The Royal Warrior")
+                await member.remove_roles(role)
+                await self.bot.get_channel(867995199161397289).send(f'{member} was deknighted The Royal Warrior')
+            elif emoji == 'spinningminecraftsteve':
+                role = discord.utils.get(member.guild.roles, name="Pixel Protector")
+                await member.remove_roles(role)
+                await self.bot.get_channel(867995199161397289).send(f'{member} was deknighted Pixel Protector')
+            elif emoji == 'PikaPickaxe':
+                role = discord.utils.get(member.guild.roles, name="Bloody Miner")
+                await member.remove_roles(role)
+                await self.bot.get_channel(867995199161397289).send(f'{member} was deknighted Bloody Miner')
 
 
 def setup(bot):
